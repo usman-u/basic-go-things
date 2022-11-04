971632e7577904540f2f8852a127c910727e70e4
Usman <unumer@hotmail.com>
2022-11-04 22:58:47 +0000

    add for loops

--- hello-world.go
+++ hello-world.go
@@ -8,14 +8,38 @@ func printer(data string) {
 	fmt.Println(data)
 }
 
+func loopy() {
+	for i := 1; i < 11; i++ {
+		fmt.Println(i)
+	}
+}
+
+func loopy2(max int) {
+	i := 1
+	for i < max {
+		fmt.Println(i)
+		i = i + 1
+	}
+}
+
+func loopy3() {
+	for i := 3; i < 20; i++ {
+		fmt.Print("shit ")
+	}
+}
+
 func main() {
 	const constant_value string = "This is a constant string"
 
-	message1 := fmt.Sprint("Hello")
-	message0 := fmt.Sprint(123)
+	// message1 := fmt.Sprint("Hello")
+	// message0 := fmt.Sprint(123)
 
-	fmt.Println(message1, message0)
+	// fmt.Println(message1, message0)
 
-	printer(constant_value)
+	// printer(constant_value)
+
+	loopy()
+	loopy2(12)
+	loopy3()
 
 }
